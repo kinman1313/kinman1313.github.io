2e340c3bf66022ce229aee044cb2777343310c61
Kevin Inman <kevin.inman@gmail.com>
2025-02-07 06:23:42 -0600

    Update app.js

--- _js/app.js
+++ _js/app.js
@@ -1,25 +1,25 @@
-setTimeout(function() {
+setTimeout(() => {
   fadeOutPreloader(document.getElementById('preloader'), 69);
 }, 1500);
 
-$(document).ready(function() {
-  $(window).on('beforeunload', function() {
+$(document).ready(() => {
+  $(window).on('beforeunload', () => {
     window.scrollTo(0, 0);
   });
 
-  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-  particlesJS.load('landing', 'assets/particles.json', function() {});
+  // Load particles
+  particlesJS.load('landing', 'assets/particles.json', () => {});
 
   // Typing Text
-  var element = document.getElementById('txt-rotate');
-  var toRotate = element.getAttribute('data-rotate');
-  var period = element.getAttribute('data-period');
-  setTimeout(function() {
+  const element = document.getElementById('txt-rotate');
+  const toRotate = element.getAttribute('data-rotate');
+  const period = element.getAttribute('data-period');
+  setTimeout(() => {
     new TxtRotate(element, JSON.parse(toRotate), period);
   }, 1500);
 
-  // INJECT CSS
-  var css = document.createElement('style');
+  // Inject CSS
+  const css = document.createElement('style');
   css.type = 'text/css';
   css.innerHTML = '#txt-rotate > .wrap { border-right: 0.08em solid #666 }';
   document.body.appendChild(css);
@@ -38,82 +38,86 @@ $(document).ready(function() {
 });
 
 /* FUNCTIONS */
-/* Preloader */
 
+// Preloader
 function fadeOutPreloader(element, duration) {
-  opacity = 1;
+  try {
+    let opacity = 1;
+    function fade() {
+      if (opacity <= 0) {
+        element.style.zIndex = 0;
+        element.style.opacity = 0;
+        element.style.filter = 'alpha(opacity = 0)';
 
-  interval = setInterval(function() {
-    if (opacity <= 0) {
-      element.style.zIndex = 0;
-      element.style.opacity = 0;
-      element.style.filter = 'alpha(opacity = 0)';
+        // Allow horizontal scroll
+        document.documentElement.style.overflowY = 'auto';
 
-      // Allow horizontal scroll
-      document.documentElement.style.overflowY = 'auto';
+        // Remove preloader div
+        document.getElementById('preloader').remove();
 
-      // Remove preloader div
-      document.getElementById('preloader').remove();
-
-      clearInterval(interval);
-    } else {
-      opacity -= 0.1;
-      element.style.opacity = opacity;
-      element.style.filter = 'alpha(opacity = ' + opacity * 100 + ')';
+        clearInterval(interval);
+      } else {
+        opacity -= 0.1;
+        element.style.opacity = opacity;
+        element.style.filter = 'alpha(opacity = ' + opacity * 100 + ')';
+        requestAnimationFrame(fade);
+      }
     }
-  }, duration);
+    fade();
+  } catch (error) {
+    console.error('Error in fadeOutPreloader: ', error);
+  }
 }
 
-/* Typing Text */
-
-var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
-};
-
-TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
-  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-
-  var that = this;
-  var delta = 200 - Math.random() * 100;
-
-  if (this.isDeleting) {
-    delta /= 5;
-  }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
+// Typing Text
+class TxtRotate {
+  constructor(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
     this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
+    this.tick();
   }
 
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
+  tick() {
+    const i = this.loopNum % this.toRotate.length;
+    const fullTxt = this.toRotate[i];
 
-/* Word Cloud */
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+    this.el.innerHTML = `<span class="wrap">${this.txt}</span>`;
 
+    let delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) {
+      delta /= 5;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+
+    setTimeout(() => {
+      this.tick();
+    }, delta);
+  }
+}
+
+// Word Cloud
 function randomizeOrder() {
-  var parent = document.getElementById('skills');
-  var divs = parent.getElementsByTagName('div');
-  var frag = document.createDocumentFragment();
+  const parent = document.getElementById('skills');
+  const divs = parent.getElementsByTagName('div');
+  const frag = document.createDocumentFragment();
 
   // Randomize order of skills
   while (divs.length) {
